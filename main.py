eb082aaaf1749d04514f56f972504bf3333ebbb5
Ben Carter <bcarter@tandom.co.uk>
2024-11-19 13:12:39 +0000

    completed

--- main.py
+++ main.py
@@ -1,27 +1,53 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    count = word_count(text)
-    c_count = character_count(text)
-    print(c_count)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-def word_count(book):
-    word = book.split()
-    word_count=len(word)
-    return(word_count)
-
-def character_count(book):
-    character_dict={}
-    for c in book:
-        lowered = c.lower()
-        if lowered in character_dict:
-            character_dict[lowered] += 1
-        else:
-            character_dict[lowered] = 1
-    return(character_dict)    
 
 main()
